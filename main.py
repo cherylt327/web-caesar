95f030eaa36add47fb95d0889c4656c660032e74
cherylt327 <cherylt327@yahoo.com>
2018-08-28 21:28:24 -0500

    Final

--- main.py
+++ main.py
@@ -10,19 +10,19 @@ form = """
 <html>
     <head>
         <style>
-            form {
+            form {{
                 background-color: #eee;
                 padding: 20px;
                 margin: 0 auto;
                 width: 540px;
                 font: 16px sans-serif;
                 border-radius: 10px;
-            }
-            textarea {
+            }}
+            textarea {{
                 margin: 10px 0;
                 width: 540px;
                 height: 120px;
-            }
+            }}
         </style>
     </head>
     <body>
@@ -30,7 +30,7 @@ form = """
         <label>Rotate by:
           <input type="text" name="rot"  value="0"/>
         </label>  
-        <textarea name="text"></textarea>
+        <textarea name="text">{0}</textarea>
         <input type="submit" />
       </form>
     </body>
@@ -42,7 +42,7 @@ form = """
 
 @app.route("/")
 def index():
-    return form
+    return form.format(...)
 
 
 @app.route("/", methods = ["POST"])
@@ -50,7 +50,7 @@ def encrypt():
   rotate = int(request.form["rot"])
   txt=request.form["text"]
   encrypted= '<h1>'+rotate_string(txt, rotate)+'</h1>'
-  return encrypted
+  return encrypted.format(...)
 
 
     
